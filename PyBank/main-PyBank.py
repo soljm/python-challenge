074a966b55e6a74fd3ebf2fa3fcdffdefe96f0b6
soljm <vee.bwii27@gmail.com>
2024-03-09 20:21:08 +1100

    refining

--- PyBank/main-PyBank.py
+++ PyBank/main-PyBank.py
@@ -1,14 +1,14 @@
 import os
 import csv
-# Path to file
-budget_data_csv = os.path.join("PyBank", "Resources", "budget_data.csv")
-# Open and read CSV file
-with open(budget_data_csv, encoding='UTF-8') as csvfile:
+
+# open and read CSV file
+with open("PyBank/Resources/budget_data.csv", encoding='UTF-8') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
-    # Skip header
+    
+    # skip header
     csv_header = next(csvfile)
-    # print(f"Header: {csv_header}")
-    # Set initial values
+
+    # set initial values
     total_months = 0
     net_profit = 0
     first_month_profit = None
@@ -18,37 +18,43 @@ with open(budget_data_csv, encoding='UTF-8') as csvfile:
     greatest_decrease = None
     greatest_increase_date = None
     greatest_decrease_date = None
-    
+
+    # loop through the rows, assigning date and profit to their respective columns
     for date, profit in csv_reader:
-        # Total Months
+        # counting total months
         total_months += 1
-        # Net Profit
+
+        # calculating net profit
         profit = int(profit)
         net_profit += profit
-        # Average change
+
+        # finding the profit for the first month and the last month
         final_month_profit = profit
         if first_month_profit is None:
             first_month_profit = profit
 
-        # Calculate profit change per month
+        # calculate profit change per month
         if last_month_profit is not None:
             current_change = profit - last_month_profit
-            # Greatest increase
+
+            # greatest increase
             if greatest_increase is None or (current_change > greatest_increase):
                 greatest_increase = current_change
                 greatest_increase_date = date
-            # Greatest decrease
+
+            # greatest decrease
             if greatest_decrease is None or (current_change < greatest_decrease):
                 greatest_decrease = current_change
                 greatest_decrease_date = date
 
+        # set current profit 
         last_month_profit = profit
-        
-        # print(f"{date=}, {profit=}, {last_month_profit=}, {current_change=}")
 
+# calculating average change and rounding to 2 decimal places
 average_change = (final_month_profit - first_month_profit) / (total_months - 1)
 average_change = round(average_change, 2)
 
+# creating a new text file and writing the analysis
 with open("PyBank/Analysis/output.txt", "w") as f:
     f.write("Financial Analysis\n")
     f.write("-------------------------------------\n")
@@ -58,6 +64,7 @@ with open("PyBank/Analysis/output.txt", "w") as f:
     f.write(f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase}\n")
     f.write(f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease}")
 
+# reading the text file to print in terminal
 with open("PyBank/Analysis/output.txt", "r") as f:
     for line in f:
         print(line)
--- PyPoll/main-Pypoll.py
+++ PyPoll/main-Pypoll.py
@@ -1,19 +1,28 @@
 import os
 import csv
 
+# open and read CSV file
 with open("PyPoll/Resources/election_data.csv", encoding='UTF-8') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
+
+    # skip header
     csv_header = next(csvfile)
-    # Set inital value
+
+    # set inital value
     total_votes = 0
     candidate_votes = {}
 
+    # looping through each row, assigning candidate to the last column and ignoring the first two
     for _, _, candidate in csv_reader:
 
+        # counting total votes
         total_votes += 1
 
+        # adding names to dictionary and adding 1 to the count each time it appears
+        # if not already in dictionary, add a new key and increase value/count to 1
         candidate_votes[candidate] = candidate_votes.get(candidate, 0) + 1
 
+# make a text file and write analysis
 with open("PyPoll/Analysis/output2.txt", 'w') as f:
     f.write("Election Results\n")
     f.write("------------------------------\n")
@@ -21,12 +30,14 @@ with open("PyPoll/Analysis/output2.txt", 'w') as f:
     f.write("------------------------------\n")
 
     winning_candidate = None
-
+    
+    # looping through each pair in dictionary to determine percentage vote
     for candidate, votes in candidate_votes.items():
         percentage = (votes / total_votes) * 100
         percentage = round(percentage, 3)
         f.write(f"{candidate}: {percentage}% ({votes})\n")
 
+        # assigning value to winning_candidate
         if winning_candidate is None or votes > candidate_votes[winning_candidate]:
             winning_candidate = candidate
 
@@ -34,6 +45,7 @@ with open("PyPoll/Analysis/output2.txt", 'w') as f:
     f.write(f"Winner: {winning_candidate}\n")
     f.write("------------------------------\n")
 
+# read text file to print to terminal
 with open("PyPoll/Analysis/output2.txt", 'r') as f:
     for line in f:
         print(line)
