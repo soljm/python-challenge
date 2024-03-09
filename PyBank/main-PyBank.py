a1e58ae92c5976180fcebd9f44977b0747fe7aa3
soljm <vee.bwii27@gmail.com>
2024-03-09 15:15:29 +1100

    implemented PyBank

---
+++ PyBank/main-PyBank.py
@@ -0,0 +1,71 @@
+import os
+import csv
+# Path to file
+budget_data_csv = os.path.join("PyBank", "Resources", "budget_data.csv")
+# Print title
+print("Financial Analysis")
+print("-------------------------------------")
+# Open and read CSV file
+with open(budget_data_csv, encoding='UTF-8') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    # Skip header
+    csv_header = next(csvfile)
+    # print(f"Header: {csv_header}")
+    # Set initial values
+    total_months = 0
+    net_profit = 0
+    first_month_profit = None
+    final_month_profit = None
+    last_month_profit = None
+    greatest_increase = None
+    greatest_decrease = None
+    greatest_increase_date = None
+    greatest_decrease_date = None
+    
+    for line in csv_reader:
+        # Total Months
+        total_months += 1
+        # Net Profit
+        date, profit = line
+        profit = int(profit)
+        net_profit += profit
+        # Average change
+        final_month_profit = profit
+        if first_month_profit is None:
+            first_month_profit = profit
+
+        # Calculate profit change per month
+        if last_month_profit is not None:
+            current_change = profit - last_month_profit
+            # Greatest increase
+            if greatest_increase is None or (current_change > greatest_increase):
+                greatest_increase = current_change
+                greatest_increase_date = date
+            # Greatest decrease
+            if greatest_decrease is None or (current_change < greatest_decrease):
+                greatest_decrease = current_change
+                greatest_decrease_date = date
+
+        last_month_profit = profit
+        
+        # print(f"{date=}, {profit=}, {last_month_profit=}, {current_change=}")
+
+average_change = (final_month_profit - first_month_profit) / (total_months - 1)
+average_change = round(average_change, 2)
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_profit}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase}")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease}")
+
+with open("PyBank/Analysis/output.txt", "w") as f:
+    f.write("Financial Analysis\n")
+    f.write("-------------------------------------\n")
+    f.write(f"Total Months: {total_months}\n")
+    f.write(f"Total: ${net_profit}\n")
+    f.write(f"Average Change: ${average_change}\n")
+    f.write(f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase}\n")
+    f.write(f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease}")
+
+
+    
